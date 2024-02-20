e1dfd71e22ce340a068e2b2a27cfe6dde26bcf9a
empostigo <emmanuel.postigo@tutanota.com>
2024-02-20 20:25:39 +0100

    Create tables for every months of a calendar

--- src/utils/fillCalendar.js
+++ src/utils/fillCalendar.js
@@ -1,25 +1,26 @@
 // Arrays for week days and months
 const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-const months = (year) => {
-  return {
-    "January": 31,
-    "February": getFebruaryNbOfDays(year),
-    "March": 31,
-    "April": 30,
-    "May": 31,
-    "June": 30,
-    "July": 31,
-    "August": 31,
-    "September": 30,
-    "October": 31,
-    "November": 30,
-    "December": 31
-  }
+const getMonths = (year) => {
+  return [
+    { month: "January", days: 31 },
+    { month: "February", days: getFebruaryNbOfDays(year) },
+    { month: "March", days: 31 },
+    { month: "April", days: 30 },
+    { month: "May", days: 31 },
+    { month: "June", days: 30 },
+    { month: "July", days: 31 },
+    { month: "August", days: 31 },
+    { month: "September", days: 30 },
+    { month: "October", days: 31 },
+    { month: "November", days: 30 },
+    { month: "December", days: 31 }
+  ]
 }
 
 export const getFebruaryNbOfDays = (year) => (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) ? 29 : 28
 
 export const getFirstDayOfTheMonth = (date) => {
+
   if (date.getDate() === 1) return date.getDay()
 
   const month = date.getMonth()
@@ -28,32 +29,25 @@ export const getFirstDayOfTheMonth = (date) => {
   return new Date(year, month, 1).getDay()
 }
 
-export const getFirstDayOfTheYear = (year) => new Date(year, 0, 1).getDay()
-
-export const linearCalendar = (year) => {
-  const yearArray = months(year)
-
-  let daysArray = []
-  Object.values(yearArray).forEach(nbDays => {
-    for (let i = 1; i <= nbDays; i++) daysArray.push(i)
-  })
-
-  return daysArray
-}
-
 export const createMonthsCalendars = (year) => {
   const NB_DAYS = 42
-  const nbOfDaysInMonths = month(year)
+  const months = getMonths(year)
+  let flatMonth = []
 
-  const monthsArray = {}
-  for (let month in nbOfDaysInMonths) {
-    const firstDayOfTheMonth = getFirstDayOfTheMonth(year,)
-    const flatCalendar = linearCalendar(year)
+  let monthsArray = {}
+  for (let month in months) {
+    const firstDayOfTheMonth = getFirstDayOfTheMonth(new Date(year, month, 1))
 
-    let flatMonth = []
-    const firstDayOfJanuaryTable = nbOfDaysInMonth.December - firstJanuary + 1
-    for (let i = firstDayOfJanuaryTable; i <= nbOfDaysInMonth.December; i++) flatMonth.push(i)
-    for (let i = firstJanuary; i < NB_DAYS; i++) flatMonth.push(flatCalendar.shift())
+    const nbOfPreviousDaysMonth = (months[month].month === "January" ? 31 : months[month - 1].days)
+    const firstDayOfMonthTable = nbOfPreviousDaysMonth - (firstDayOfTheMonth === 0 ? 7 : firstDayOfTheMonth) + 1
+    for (let i = firstDayOfMonthTable; i <= nbOfPreviousDaysMonth; i++) flatMonth.push(i)
+    for (let i = 1; i <= months[month].days; i++) flatMonth.push(i)
+    const leftToFill = NB_DAYS - flatMonth.length
+    for (let i = 1; i <= leftToFill; i++) flatMonth.push(i)
+
+    monthsArray[months[month].month] = flatMonth
+    flatMonth = []
   }
 
+  return monthsArray
 }
