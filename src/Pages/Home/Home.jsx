fab86f8a218190ffef09cb841c78d410c3b24851
empostigo <emmanuel.postigo@tutanota.com>
2024-03-17 15:05:19 +0100

    Add data

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,6 +9,9 @@ import Select from "../../components/Select/Select"
 import UList from "../../components/UList/UList"
 import DatePicker from "../../components/DatePicker/DatePicker"
 
+// Data
+import { states } from "../../data/states"
+
 // Style
 import homeStyle from "./Home.module.scss"
 
@@ -18,18 +21,50 @@ const Home = () => {
     setValue(value)
   }
 
-  const test = []
-  for (let i = 0; i < 16; i++) test.push(i)
+
+  const departments = ["Sales", "Marketing", "Engineering", "Human Resources", "Legal"]
 
   return (
-    <main className={homeStyle.main}>
-      <h1>HRnet</h1>
-      <Link to="/employees">View Current Employees</Link>
-      <DatePicker name="birthDate" />
-      <Select initValue={value} onValueChange={onValueChange}>
-        <UList list={test} />
-      </Select>
-    </main>
+    <>
+      <header className={homeStyle.header}><h1>HRnet</h1></header>
+      <main className={homeStyle.main}>
+        <Link to="/employees">View Current Employees</Link>
+        <h2>Create Employee</h2>
+        <form action="#" id="create-employee" className={homeStyle.form}>
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" />
+
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" />
+
+          <DatePicker name="birthDate" label="Date of Birth" />
+          <DatePicker name="startDate" label="Start Date" />
+
+          <fieldset className={homeStyle.fieldset}>
+            <legend>Address</legend>
+
+            <label htmlFor="street">Street</label>
+            <input id="street" type="text" />
+
+            <label htmlFor="city">City</label>
+            <input id="city" type="text" />
+
+            <label htmlFor="state">State</label>
+            <Select initValue={states[0].name} onValueChange={setValue}>
+              <UList list={states.map(state => state.name)} />
+            </Select>
+
+            <label htmlFor="zip-code">Zip Code</label>
+            <input id="zip-code" type="number" />
+          </fieldset>
+
+          <label htmlFor="department">Department</label>
+          <Select initValue={departments[0]} onValueChange={setValue}>
+            <UList list={departments} />
+          </Select>
+        </form>
+      </main>
+    </>
   )
 }
 
