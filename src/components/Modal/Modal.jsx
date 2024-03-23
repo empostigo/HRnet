c2dab62370d3edc8d93df4a8188fc5bd19a5abb7
empostigo <emmanuel.postigo@tutanota.com>
2024-03-23 09:18:56 +0100

    Enable to close modal with escape key

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,5 @@
 // React
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 // assets
 import closing from "../../assets/closing.svg"
@@ -8,6 +8,16 @@ import closing from "../../assets/closing.svg"
 import modalStyle from "./Modal.module.scss"
 
 const Modal = ({ message, closeModal }) => {
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.key === "Escape") closeModal()
+    }
+
+    document.addEventListener("keydown", handleKeyDown)
+
+    return () => document.removeEventListener("keydown", handleKeyDown)
+  }, [closeModal])
+
   return (
     <div className={modalStyle.container}>
       <div className={modalStyle.wrapper}>
